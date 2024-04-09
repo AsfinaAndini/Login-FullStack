22835ebcabf29f64079285c81e2a474c08bdc8ea
AsfinaAndini <128123181+AsfinaAndini@users.noreply.github.com>
2024-04-09 11:38:47 +0700

    frist commit

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {NavLink} from "react-router-dom"
+
+const Sidebar = () => {
+  return (
+    <div>
+  <aside className="menu has-shadow">
+  <p className="menu-label">General</p>
+  <ul className="menu-list">
+    <li><NavLink to = {"/dashboard"}>Dashboard</NavLink></li>
+    <li><NavLink to = {"/products"}>Product</NavLink></li>
+  </ul>
+  <p className="menu-label">Admin</p>
+  <ul className="menu-list">
+    <li><NavLink to = {"/users"}>Users</NavLink></li>
+    </ul>
+  <p className="menu-label">Transactions</p>
+  <ul className="menu-list">
+    <li><a>Payments</a></li>
+    <li><a>Transfers</a></li>
+    <li><a>Balance</a></li>
+  </ul>
+</aside>
+    </div>
+  )
+}
+
+export default Sidebar
