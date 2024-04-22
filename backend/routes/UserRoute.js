fe6c64af9ecc447454e1e7c0109b3ef76c18db92
AsfinaAndini <128123181+AsfinaAndini@users.noreply.github.com>
2024-04-22 19:57:09 +0700

    fixed user route

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -13,7 +13,7 @@ const router = express.Router();
 router.get('/users', verifyUser, adminOnly, getUsers);
 router.get('/users/:id',  verifyUser, adminOnly, getUserById);
 router.post('/users',  verifyUser, adminOnly, createUser);
-router.put('/users/:id',  verifyUser, adminOnly, updateUser);
+router.patch('/users/:id',  verifyUser, adminOnly, updateUser);
 router.delete('/users/:id',  verifyUser, adminOnly, deleteUser);
 
 export default router;
